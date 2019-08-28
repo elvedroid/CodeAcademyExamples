c0526c2eee6ae32cf86e90f70fce98eef723bb59
Elvedin Selimoski <es@medialab.io>
2019-08-29 00:01:09 +0200

    Fragments/Pass data to fragment

--- Fragments/app/src/main/java/com/example/fragments/FirstFragment.java
+++ Fragments/app/src/main/java/com/example/fragments/FirstFragment.java
@@ -12,6 +12,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import static com.example.fragments.MainActivity.NAME_EXTRA;
+
 public class FirstFragment extends Fragment {
 
     public FirstFragment() {
@@ -30,18 +32,21 @@ public class FirstFragment extends Fragment {
 //        Тука следуваат сите поставувања на фрагментот и сите негови View-а.
 
 //        Моментално не се користи
-        final EditText editText = rootView.findViewById(R.id.etName);
+        final EditText etName = rootView.findViewById(R.id.etName);
 
         Button button = rootView.findViewById(R.id.btnOpenSecondActivity);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 SecondFragment secondFragment = new SecondFragment();
+                Bundle args = new Bundle();
+                args.putString(NAME_EXTRA, etName.getText().toString());
+                secondFragment.setArguments(args);
 //                getFragmentManager() го враќа истиот фрагмент менаџер како и
 //                getSupportFragmentManager() во MainActivity класата
                 FragmentManager fragmentManager = getFragmentManager();
 //                Проверуваме дали постои фрагмент менаџерот. Во фрагментите е добро да се прави оваа
-//                проверка затоа што има ситуааци кога getFragmentManager() враќа null.
+//                проверка затоа што има ситуации кога getFragmentManager() враќа null.
                 if (fragmentManager != null) {
 //                    Тука го заменуваме првиот фрагмент со вториот, и вториот фрагмент го додаваме
 //                    во BackStack-от од фрагмент менаџерот(Најчесто корисно за правилно
--- Fragments/app/src/main/java/com/example/fragments/SecondFragment.java
+++ Fragments/app/src/main/java/com/example/fragments/SecondFragment.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import static com.example.fragments.MainActivity.NAME_EXTRA;
 
 
 /**
@@ -24,7 +27,17 @@ public class SecondFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_second, container, false);
+        TextView tvName = rootView.findViewById(R.id.tvName);
+//        Проверуваме дали постојат аргументите
+        if (getArguments() != null) {
+//            Ова е идентично како и getStringExtra() кај интентинте.
+//            Вториот аргумент од getString методот е опционален и е дефолтна вредност ако не постои
+//            стринг со дадениот клуч.
+            String name = getArguments().getString(NAME_EXTRA, "");
+            tvName.setText(name);
+        }
+        return rootView;
     }
 
 }
